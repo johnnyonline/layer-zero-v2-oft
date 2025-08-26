7b86c7949a954b776cf56fe0ff35493d79c1a3ea
johnnyonline <johnnyonline.eth@gmail.com>
2025-08-26 15:04:50 -0300

    chore: add some stuff

--- script/1.DeployOFTAdapter.s.sol
+++ script/1.DeployOFTAdapter.s.sol
@@ -1,9 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.22;
 
+import {SetConfigParam} from "@layerzerolabs/lz-evm-protocol-v2/contracts/interfaces/IMessageLibManager.sol";
 import {ILayerZeroEndpointV2} from "@layerzerolabs/lz-evm-protocol-v2/contracts/interfaces/ILayerZeroEndpointV2.sol";
 import {UlnConfig} from "@layerzerolabs/lz-evm-messagelib-v2/contracts/uln/UlnBase.sol";
 import {ExecutorConfig} from "@layerzerolabs/lz-evm-messagelib-v2/contracts/SendLibBase.sol";
+import {EnforcedOptionParam} from "@layerzerolabs/oapp-evm/contracts/oapp/libs/OAppOptionsType3.sol";
+import {OptionsBuilder} from "@layerzerolabs/oapp-evm/contracts/oapp/libs/OptionsBuilder.sol";
 
 import {ASFOFTAdapter} from "../src/OFTAdapter.sol";
 
@@ -17,12 +20,19 @@ import "forge-std/Script.sol";
 // forge script script/1.DeployOFTAdapter.s.sol:DeployOFTAdapter --verify --slow --legacy --etherscan-api-key $KEY --rpc-url $ETH_RPC_URL --broadcast
 
 contract DeployOFTAdapter is Script {
+    using OptionsBuilder for bytes;
+
     address public constant ASF = address(0x59a529070fBb61e6D6c91f952CcB7f35c34Cf8Aa); // ASF Mainnet
     address public constant ENDPOINT = address(0x1a44076050125825900e736c501f859c50fE728c); // Ethereum Mainnet endpointV2
     address public constant DEPLOYER = address(0x6969acca95B7fb9631a114085eEEBd161EC19f25); // asym deployer
     address public constant ETH_SENDULN302 = address(0xbB2Ea70C9E858123480642Cf96acbcCE1372dCe1); // SendUln302 Ethereum
     address public constant ETH_RECEIVEULN302 = address(0xc02Ab410f0734EFa3F14628780e6e695156024C2); // ReceiveUln302 Ethereum
+    address public constant ETH_CANARY_DVN = address(0xa4fE5A5B9A846458a70Cd0748228aED3bF65c2cd); // Ethereum Canary DVN
+    address public constant ETH_DEUTCHE_DVN = address(0x373a6E5c0C4E89E24819f00AA37ea370917AAfF4); // Ethereum Deutsche Telekom DVN
+    address public constant ETH_LUGANODES_DVN = address(0x58249a2Ec05c1978bF21DF1f5eC1847e42455CF4); // Ethereum Luganodes DVN
+    address public constant ETH_LZ_EXECUTOR = address(0x173272739Bd7Aa6e4e214714048a9fE699453059); // LZ Executor ethereum
 
+    uint16 SEND = 1;  // Message type for sendString function
     uint32 constant EXECUTOR_CONFIG_TYPE = 1;
     uint32 constant ULN_CONFIG_TYPE = 2;
     uint32 public constant BASE_EID = 30184; // Base Mainnet
@@ -40,6 +50,9 @@ contract DeployOFTAdapter is Script {
 
         // Set send config
         _setSendConfig(_oftAdapter);
+
+        // Set enforced options
+        _setEnforcedOptions(_oftAdapter);
 
         vm.stopBroadcast();
 
@@ -68,29 +81,50 @@ contract DeployOFTAdapter is Script {
     }
 
     function _setSendConfig(address _oftAdapter) internal {
-        // UlnConfig memory uln = UlnConfig({
-        //     confirmations: 15, // minimum block confirmations required on A before sending to B
-        //     requiredDVNCount: 2, // number of DVNs required
-        //     optionalDVNCount: type(uint8).max, // optional DVNs count, uint8
-        //     optionalDVNThreshold: 0, // optional DVN threshold
-        //     requiredDVNs: [address(0x1111...), address(0x2222...)], // sorted list of required DVN addresses
-        //     optionalDVNs: [] // sorted list of optional DVNs
-        // });
+        address[] memory requiredDVNs = new address[](0);
+        address[] memory optionalDVNs = new address[](3);
+        optionalDVNs[0] = ETH_CANARY_DVN;
+        optionalDVNs[1] = ETH_DEUTCHE_DVN;
+        optionalDVNs[2] = ETH_LUGANODES_DVN;
+        UlnConfig memory uln = UlnConfig({
+            confirmations: 15, // minimum block confirmations required on A before sending to B
+            requiredDVNCount: 0, // number of DVNs required
+            optionalDVNCount: type(uint8).max, // optional DVNs count, uint8
+            optionalDVNThreshold: 2, // optional DVN threshold
+            requiredDVNs: requiredDVNs, // sorted list of required DVN addresses
+            optionalDVNs: optionalDVNs // sorted list of optional DVNs
+        });
 
-        // ExecutorConfig memory exec = ExecutorConfig({
-        //     maxMessageSize: 10000, // max bytes per cross-chain message
-        //     executor: address(0x3333...) // address that pays destination execution fees on B
-        // });
+        ExecutorConfig memory exec = ExecutorConfig({
+            maxMessageSize: 10000, // max bytes per cross-chain message
+            executor: ETH_LZ_EXECUTOR // address that pays destination execution fees on B
+        });
 
-        // bytes memory encodedUln  = abi.encode(uln);
-        // bytes memory encodedExec = abi.encode(exec);
+        bytes memory encodedUln  = abi.encode(uln);
+        bytes memory encodedExec = abi.encode(exec);
 
-        // SetConfigParam[] memory params = new SetConfigParam[](2);
-        // params[0] = SetConfigParam(BASE_EID, EXECUTOR_CONFIG_TYPE, encodedExec);
-        // params[1] = SetConfigParam(BASE_EID, ULN_CONFIG_TYPE, encodedUln);
+        SetConfigParam[] memory params = new SetConfigParam[](2);
+        params[0] = SetConfigParam(BASE_EID, EXECUTOR_CONFIG_TYPE, encodedExec);
+        params[1] = SetConfigParam(BASE_EID, ULN_CONFIG_TYPE, encodedUln);
 
-        // // Set config for messages sent from A to B
-        // ILayerZeroEndpointV2(ENDPOINT).setConfig(_oftAdapter, ETH_SENDULN302, params);
-        return; // Use default
+        // Set config for messages sent from A to B
+        ILayerZeroEndpointV2(ENDPOINT).setConfig(_oftAdapter, ETH_SENDULN302, params);
+    }
+
+    function _setEnforcedOptions(address _oftAdapter) internal {
+        bytes memory options = OptionsBuilder.newOptions().addExecutorLzReceiveOption(100000, 0);
+
+        // Create enforced options array
+        EnforcedOptionParam[] memory enforcedOptions = new EnforcedOptionParam[](1);
+
+        // Set enforced options for first destination
+        enforcedOptions[0] = EnforcedOptionParam({
+            eid: BASE_EID,
+            msgType: SEND,
+            options: options
+        });
+
+        // Set enforced options on the OApp
+        ASFOFTAdapter(_oftAdapter).setEnforcedOptions(enforcedOptions);
     }
 }
--- script/2.DeployOFT.s.sol
+++ script/2.DeployOFT.s.sol
@@ -1,9 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.22;
 
+import {SetConfigParam} from "@layerzerolabs/lz-evm-protocol-v2/contracts/interfaces/IMessageLibManager.sol";
 import {ILayerZeroEndpointV2} from "@layerzerolabs/lz-evm-protocol-v2/contracts/interfaces/ILayerZeroEndpointV2.sol";
 import {UlnConfig} from "@layerzerolabs/lz-evm-messagelib-v2/contracts/uln/UlnBase.sol";
 import {ExecutorConfig} from "@layerzerolabs/lz-evm-messagelib-v2/contracts/SendLibBase.sol";
+import {EnforcedOptionParam} from "@layerzerolabs/oapp-evm/contracts/oapp/libs/OAppOptionsType3.sol";
+import {OptionsBuilder} from "@layerzerolabs/oapp-evm/contracts/oapp/libs/OptionsBuilder.sol";
 
 import {ASFOFT} from "../src/OFT.sol";
 
@@ -17,6 +20,8 @@ import "forge-std/Script.sol";
 // forge script script/2.DeployOFT.s.sol:DeployOFT --verify --slow --legacy --etherscan-api-key $KEY --rpc-url $BASE_RPC_URL --broadcast
 
 contract DeployOFT is Script {
+    using OptionsBuilder for bytes;
+
     string public constant NAME = "Asymmetry Finance Token";
     string public constant SYMBOL = "ASF";
 
@@ -24,7 +29,12 @@ contract DeployOFT is Script {
     address public constant DEPLOYER = address(0x6969acca95B7fb9631a114085eEEBd161EC19f25);
     address public constant BASE_SENDULN302 = address(0xB5320B0B3a13cC860893E2Bd79FCd7e13484Dda2); // SendUln302 Base
     address public constant BASE_RECEIVEULN302 = address(0xc70AB6f32772f59fBfc23889Caf4Ba3376C84bAf); // ReceiveUln302 Base
+    address public constant BASE_CANARY_DVN = address(0x554833698Ae0FB22ECC90B01222903fD62CA4B47); // Base Canary DVN
+    address public constant BASE_DEUTCHE_DVN = address(0xc2A0C36f5939A14966705c7Cec813163FaEEa1F0); // Base Deutsche Telekom DVN
+    address public constant BASE_LUGANODES_DVN = address(0xa0AF56164F02bDf9d75287ee77c568889F11d5f2); // Base Luganodes DVN
+    address public constant ETH_LZ_EXECUTOR = address(0x173272739Bd7Aa6e4e214714048a9fE699453059); // LZ Executor ethereum
 
+    uint16 SEND = 1;  // Message type for sendString function
     uint32 constant RECEIVE_CONFIG_TYPE = 2;
     uint32 public constant BASE_EID = 30184; // Base Mainnet
     uint32 public constant ETH_EID = 30101; // Ethereum Mainnet
@@ -41,6 +51,9 @@ contract DeployOFT is Script {
 
         // Set receive config
         _setReceiveConfig(_oft);
+
+        // Set enforced options
+        _setEnforcedOptions(_oft);
 
         vm.stopBroadcast();
 
@@ -69,22 +82,42 @@ contract DeployOFT is Script {
     }
 
     function _setReceiveConfig(address _oft) internal {
-        // UlnConfig memory uln = UlnConfig({
-        //     confirmations: 15, // min block confirmations from source (A)
-        //     requiredDVNCount: 2, // required DVNs for message acceptance
-        //     optionalDVNCount: type(uint8).max, // optional DVNs count
-        //     optionalDVNThreshold: 0, // optional DVN threshold
-        //     requiredDVNs: [address(0x1111...), address(0x2222...)], // sorted required DVNs
-        //     optionalDVNs: [] // no optional DVNs
-        // });
+        address[] memory requiredDVNs = new address[](0);
+        address[] memory optionalDVNs = new address[](3);
+        optionalDVNs[0] = BASE_CANARY_DVN;
+        optionalDVNs[1] = BASE_DEUTCHE_DVN;
+        optionalDVNs[2] = BASE_LUGANODES_DVN;
+        UlnConfig memory uln = UlnConfig({
+            confirmations: 15, // min block confirmations from source (A)
+            requiredDVNCount: 0, // required DVNs for message acceptance
+            optionalDVNCount: type(uint8).max, // optional DVNs count
+            optionalDVNThreshold: 2, // optional DVN threshold
+            requiredDVNs: requiredDVNs, // sorted required DVNs
+            optionalDVNs: optionalDVNs // sorted optional DVNs
+        });
 
-        // bytes memory encodedUln = abi.encode(uln);
+        bytes memory encodedUln = abi.encode(uln);
 
-        // SetConfigParam[] memory params = new SetConfigParam[](1);
-        // params[0] = SetConfigParam(eid, RECEIVE_CONFIG_TYPE, encodedUln);
+        SetConfigParam[] memory params = new SetConfigParam[](1);
+        params[0] = SetConfigParam(ETH_EID, RECEIVE_CONFIG_TYPE, encodedUln);
 
-        // vm.startBroadcast(signer);
-        // ILayerZeroEndpointV2(endpoint).setConfig(oapp, receiveLib, params); // Set config for messages received on B from A
-        return; // Use default
+        ILayerZeroEndpointV2(ENDPOINT).setConfig(_oft, BASE_RECEIVEULN302, params); // Set config for messages received on B from A
+    }
+
+    function _setEnforcedOptions(address _oft) internal {
+        bytes memory options = OptionsBuilder.newOptions().addExecutorLzReceiveOption(100000, 0);
+
+        // Create enforced options array
+        EnforcedOptionParam[] memory enforcedOptions = new EnforcedOptionParam[](1);
+
+        // Set enforced options for first destination
+        enforcedOptions[0] = EnforcedOptionParam({
+            eid: ETH_EID,
+            msgType: SEND,
+            options: options
+        });
+
+        // Set enforced options on the OApp
+        ASFOFT(_oft).setEnforcedOptions(enforcedOptions);
     }
 }
